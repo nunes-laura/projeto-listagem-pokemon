81e7c8aaa139f8c31ab7b6c73763e87335c5ea7c
Laura Nunes <lauranunes2508@gmail.com>
2023-08-30 13:45:35 -0300

    primeira requisição

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 
-console.log('Olá, mundo!');
+const offset = 0
+const limit = 10
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offest{&limit=${limit}'
+
+
+fetch(url).then(function (response){
+    console.log(response);
+})
