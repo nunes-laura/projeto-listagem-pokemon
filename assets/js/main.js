8635526f6bdfa8c48fda2968102c8011ef513cf8
Laura Nunes <lauranunes2508@gmail.com>
2023-08-30 13:55:56 -0300

    promise - exemplo de callback

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,18 @@ const limit = 10
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offest{&limit=${limit}'
 
 
-fetch(url).then(function (response){
-    console.log(response);
+fetch(url)
+    .then(function (response){
+        response.json().then(function (responseBody){
+            console.log(responseBody);
+        })
+    
 })
+
+.catch(function (error){
+    console.error(error);
+})
+.finally(function (){
+    console.log('Requisição concluída')
+})
+
