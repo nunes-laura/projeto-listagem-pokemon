953a3f90ab6a3528b93126c32a6f90d7085fee30
Laura Nunes <lauranunes2508@gmail.com>
2023-08-31 18:52:00 -0300

    lista de pokemon em lista HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,52 @@
 
-const offset = 0
-const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offest}&limit=${limit}'
+const url = `https://pokeapi.co/api/v2/pokemon?offset=10&limit=10`
+
+
+function convertPokemonToLi(pokemon){
+    return ` <li class="pokemon">
+                    <span class="number">#001</span>
+                    <span class="name">${pokemon.name}</span>
+
+                    <div class="detail">
+                       
+                        <ol class="types">
+                            <li class="type">grass</li>
+                            <li class="type">poison</li>
+
+                        </ol>
+
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" 
+                    alt="${pokemon.name}">
+
+                    </div>
+                </li> `   
+    
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 
 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    
+    .then((pokemons) => {
+        for (let index = 0; index < pokemons.length; index++) {
+            const pokemon = pokemons[index];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            
+        }      
+        
+    })
     .catch( (error)=> console.error(error))
+
+
+
+
+
+
+
 
 
