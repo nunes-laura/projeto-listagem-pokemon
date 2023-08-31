85b5d44feb2afc6a5025494aa04be07a35d0b1b0
Laura Nunes <lauranunes2508@gmail.com>
2023-08-31 17:44:31 -0300

    removendo callback

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
 
 const offset = 0
 const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offest{&limit=${limit}'
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offest}&limit=${limit}'
 
 
 fetch(url)
     .then(function (response){
-        response.json().then(function (responseBody){
-            console.log(responseBody);
+        return response.json()
         })
     
-})
+    .then(function (jsonBody){
+        console.log(jsonBody)
+    })
 
 .catch(function (error){
     console.error(error);
