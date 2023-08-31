fae10ae4bd159e2bee68adba88e2248324e27bae
Laura Nunes <lauranunes2508@gmail.com>
2023-08-31 17:49:18 -0300

    simplificação e arrow functions na manipulaçao de resultados

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,8 @@ const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offest}&limit=${limit}'
 
 
 fetch(url)
-    .then(function (response){
-        return response.json()
-        })
-    
-    .then(function (jsonBody){
-        console.log(jsonBody)
-    })
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch( (error)=> console.error(error))
 
-.catch(function (error){
-    console.error(error);
-})
-.finally(function (){
-    console.log('Requisição concluída')
-})
 
