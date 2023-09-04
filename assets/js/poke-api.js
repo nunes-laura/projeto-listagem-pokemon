154a2143386ac5e91ad7637634a516be33006245
Laura Nunes <lauranunes2508@gmail.com>
2023-09-04 13:24:52 -0300

    revisando o código

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -7,8 +7,21 @@ const pokeApi = {
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
     return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
     .catch( (error)=> console.error(error))
 }
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4'),
+    fetch('https://pokeapi.co/api/v2/pokemon/5'),
+    fetch('https://pokeapi.co/api/v2/pokemon/6')
+
+]).then((results)=>{
+    console.log(results)
+})
